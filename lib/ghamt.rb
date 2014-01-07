2932039e16bb2fa242bcbecd94ad71feadb8471e
Joao Santos <familiareal@gmail.com>
2014-01-07 22:28:17 +0000

    bootstrap: create github client, update readme

--- lib/ghamt.rb
+++ lib/ghamt.rb
@@ -1,5 +1,15 @@
 require "ghamt/version"
+require 'rubygems'
+require 'github_api'
+require 'open-uri'
+
+ERROR_MISSING_TOKEN = 'Token missing.'
 
 module Ghamt
   puts "ghAMT #{VERSION}"
+
+  TOKEN=`git config --global --get github.token`
+  raise RuntimeError, ERROR_MISSING_TOKEN if TOKEN == nil
+
+  github = Github.new :oauth_token => TOKEN.chomp
 end
