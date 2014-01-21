199a3cdd0790e494da3894a8eb4cc9bc20164618
Joao Santos <familiareal@gmail.com>
2014-01-21 21:44:37 +0000

    refactor into manager

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,55 +1,9 @@
-def greet
-  puts "ghAMT starting"
-end
+require 'manager.rb'
 
-def exit
-  puts "ghAMT finished!"
-end
+m = Manager.new ARGV
+m.greet
 
-def show_params
-  puts "command : #{@command}"
-  puts "path    : #{@path}"
-  puts "commit  : #{@commit}"
-  puts "args    : #{@args}"
-end
-
-def latest_commit path
-  `git log -n 1 --pretty=oneline`.split(" ")[0]
-end
-
-def set_values params
-  @path ||= `pwd`
-  @command ||= params[0] if validate_command params[0]
-  @commit ||= latest_commit @path
-  @args ||= params[1..-1]
-end
-
-def validate_command command
-  case command
-    when "upload"
-      true
-    when "manage"
-      true
-    when "download"
-      true
-    when "install"
-      true
-    when "reset"
-      true
-    when "test"
-      true
-    else
-      false
-  end
-end
-
-def check_values
-	raise "invalid command" unless @command
-end
-
-greet
-set_values ARGV
-check_values
-show_params
-exit
-
+m.check_values
+m.show_params
+m.execute
+m.quit
---
+++ lib/manager.rb
@@ -0,0 +1,54 @@
+class Manager
+
+  def greet
+    puts "ghAMT starting"
+  end
+
+  def quit
+    puts "ghAMT finished!"
+  end
+
+  def show_params
+    puts "command : #{@command}"
+    puts "path    : #{@path}"
+    puts "commit  : #{@commit}"
+    puts "args    : #{@args}"
+  end
+
+  def latest_commit path
+    `git log -n 1 --pretty=oneline`.split(" ")[0]
+  end
+
+  def initialize params
+    @path ||= `pwd`
+    @command ||= params[0] if validate_command params[0]
+    @commit ||= latest_commit @path
+    @args ||= params[1..-1]
+  end
+
+  def validate_command command
+    case command
+      when "upload", "manage", "download", "install", "reset", "test"
+        true
+      else
+        false
+    end
+  end
+
+  def check_values
+    raise "invalid command" unless @command
+  end
+
+  def execute
+    case @command
+      when "upload"
+        upload self
+      when "manage", "download", "install", "reset", "test"
+        true
+    end
+  end
+
+  def upload job
+    true
+  end
+end
